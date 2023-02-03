ea837deeb7ee89e97f11ebf7411138f5ab112a42
Andre S Nascimento <andresoaresnascimento@gmail.com>
2023-02-03 00:24:25 -0300

    Controle de Acesso

--- src/main/java/com/api/parkingcontrol/configuration/security/WebSecurityConfig.java
+++ src/main/java/com/api/parkingcontrol/configuration/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.api.parkingcontrol.configuration.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -7,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -16,6 +18,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
+  @Autowired
+  UserDetailsServiceImpl userDetailsService;
+
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
     http
@@ -23,29 +28,31 @@ public class WebSecurityConfig {
         .and()
         .authorizeHttpRequests()
         .requestMatchers(HttpMethod.GET, "/parking-spot/**").permitAll()
-        .requestMatchers(HttpMethod.POST, "/parking-spot").hasRole("USER")
-        .requestMatchers(HttpMethod.DELETE, "/parking-spot/**").hasRole("ADMIN")
-        .anyRequest().fullyAuthenticated()
+//        .requestMatchers(HttpMethod.POST, "/parking-spot").hasRole("USER")
+//        .requestMatchers(HttpMethod.DELETE, "/parking-spot/**").hasRole("ADMIN")
+        .anyRequest().authenticated()
         .and()
-        .csrf().disable();
+        .csrf().disable()
+        .userDetailsService(userDetailsService);
 
     return http.build();
   }
 
-  @Bean
-  public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
-    UserDetails user = User.withUsername("user")
-        .password(passwordEncoder.encode("user"))
-        .roles("USER")
-        .build();
+//  @Bean
+//  public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+//    UserDetails user = User.withUsername("user")
+//        .password(passwordEncoder.encode("user"))
+//        .roles("USER")
+//        .build();
+//
+//    UserDetails admin = User.withUsername("admin")
+//        .password(passwordEncoder.encode("admin"))
+//        .roles("USER", "ADMIN")
+//        .build();
+//
+//    return new InMemoryUserDetailsManager(user, admin);
+//  }
 
-    UserDetails admin = User.withUsername("admin")
-        .password(passwordEncoder.encode("admin"))
-        .roles("USER", "ADMIN")
-        .build();
-
-    return new InMemoryUserDetailsManager(user, admin);
-  }
 
   @Bean
   public PasswordEncoder passwordEncoder(){
